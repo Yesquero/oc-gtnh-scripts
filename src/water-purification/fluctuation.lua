079224c2b5479472d2ff159767c0178d52d7a621
Yesquero <yesquero1986@gmail.com>
2025-05-17 17:35:03 +0300

    Tier 5 purification

--- src/water-purification/fluctuation.lua
+++ src/water-purification/fluctuation.lua
@@ -2,20 +2,40 @@ local sides = require("sides")
 local component = require("component")
 local term = require("term")
 
-local plasmaTs = component.proxy("id")
-local coolantTs = component.proxy("id")
-local controller = component.gt_machine
+local utility = require("utility")
 
-local function doStuff()
-
+local function canDoCycle(controller)
+    return controller.getWorkMaxProgress() - controller.getWorkProgress() > 30 * 20
 end
+
+-- init variables
+local plasmaTs = utility.liquidTransposer:new("Plasma TS", component.proxy("id"), sides.down, sides.top)
+local coolantTs = utility.liquidTransposer:new("Coolant TS", component.proxy("id"), sides.down, sides.top)
+local controller = component.gt_machine
 
 -- main loop
 while true do
+    term.clear()
     if controller.isMachineActive() then
-        doStuff()
+        if canDoCycle(controller) then
+            print("Adding plasma...")
+            plasmaTs.addFluid(100)
+            ---@diagnostic disable-next-line undefined-field
+            os.sleep(10)
+
+            print("Adding coolant...")
+            coolantTs.addFluid(2000)
+            ---@diagnostic disable-next-line undefined-field
+            os.sleep(20)
+        else
+            local ticksUntilnewCycle = controller.getWorkMaxProgress() - controller.getWorkProgress()
+            print("Not enough time for heating/cooling cycle, skipping; (" .. tostring(ticksUntilnewCycle) .. " ticks)")
+            ---@diagnostic disable-next-line undefined-field
+            os.sleep(ticksUntilnewCycle / 20)
+        end
     else
----@diagnostic disable-next-line: undefined-field
+        print("Waiting for machine activity...")
+        ---@diagnostic disable-next-line: undefined-field
         os.sleep(10)
     end
 end
